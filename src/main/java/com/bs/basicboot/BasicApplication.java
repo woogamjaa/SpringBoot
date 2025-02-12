4bc2d85457c0a9b7184d6b33491f3bed3e52643e
woogamjaa <woogamjaa@gmail.com>
2025-02-12 11:38:24 +0900

    spring boot - 코드로서 properties 구성해서 설정하여 실행할 수 있음.

--- src/main/java/com/bs/basicboot/BasicApplication.java
+++ src/main/java/com/bs/basicboot/BasicApplication.java
@@ -3,11 +3,25 @@ package com.bs.basicboot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Properties;
+
 @SpringBootApplication
 public class BasicApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BasicApplication.class, args);
+        //부트 프로그램 시작하는 곳
+        //시작할때 Properties를 설정해줄 수 있음.
+        Properties applicationProperties = new Properties();
+        applicationProperties.setProperty("server.port", "9099");
+
+        //SpringApplication클래스에서 제공하는 setDefaultProperties()메소드를 이용
+        SpringApplication springApplication = new SpringApplication(BasicApplication.class);
+        springApplication.setDefaultProperties(applicationProperties);
+
+        springApplication.run(args);
+
+
+//        SpringApplication.run(BasicApplication.class, args);
     }
 
 }
