62acd2981c0ed057a3290b92a67e0329127b774d
woogamjaa <woogamjaa@gmail.com>
2025-02-17 13:40:10 +0900

    spring boot - vs cors 매핑

--- src/main/java/com/bs/basicboot/common/config/properties/WebMvcConfig.java
+++ src/main/java/com/bs/basicboot/common/config/properties/WebMvcConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -46,4 +47,8 @@ public class WebMvcConfig implements WebMvcConfigurer {
                .getValidator();
     }
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedOrigins("http://localhost:52330");
+    }
 }
