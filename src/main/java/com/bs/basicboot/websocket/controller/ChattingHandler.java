40cb80f2723bc44a9f6d24d293db9257def6eacd
woogamjaa <woogamjaa@gmail.com>
2025-02-24 09:28:04 +0900

    spring boot - web소켓 설정방법

---
+++ src/main/java/com/bs/basicboot/websocket/controller/ChattingHandler.java
@@ -0,0 +1,34 @@
+package com.bs.basicboot.websocket.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Component
+@Slf4j
+public class ChattingHandler extends TextWebSocketHandler {
+
+    private Map<String,WebSocketSession> clients = new HashMap<>();
+
+    @Override
+    public void afterConnectionEstablished(WebSocketSession session) throws Exception {
+        super.afterConnectionEstablished(session);
+    }
+
+    @Override
+    public void afterConnectionClosed(WebSocketSession session, CloseStatus status) throws Exception {
+        super.afterConnectionClosed(session, status);
+    }
+
+    @Override
+    protected void handleTextMessage(WebSocketSession session, TextMessage message) throws Exception {
+        super.handleTextMessage(session, message);
+    }
+}
