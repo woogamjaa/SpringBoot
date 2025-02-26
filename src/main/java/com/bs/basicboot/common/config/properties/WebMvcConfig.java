45df780bf9f5f9fa1c4146db2d858a541af6786c
woogamjaa <woogamjaa@gmail.com>
2025-02-26 13:46:20 +0900

    spring boot - thymeleaf관련 주석 후 addResourceHandlers 메소드 추가하여 연동

--- src/main/java/com/bs/basicboot/common/config/properties/WebMvcConfig.java
+++ src/main/java/com/bs/basicboot/common/config/properties/WebMvcConfig.java
@@ -11,10 +11,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.HandlerInterceptor;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.*;
 
 @Configuration
 @EnableAspectJAutoProxy
@@ -23,6 +20,12 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @MapperScan("com.bs.basicboot.model.dao")
 
 public class WebMvcConfig implements WebMvcConfigurer {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**")
+                .addResourceLocations("classpath:/**","classpath:/templates/");
+    }
+
     @Bean
     FilterRegistrationBean<myFilter> testFilter() {
         FilterRegistrationBean<myFilter> filter= new FilterRegistrationBean<>();
